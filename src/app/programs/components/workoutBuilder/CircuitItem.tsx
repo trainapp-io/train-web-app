7d5f7f14a9f0a53d6c61f825a6a69f92983fd6c5
noahgross1 <noahgross1@gmail.com>
2026-04-17 15:30:34 -0400

    fix: default new exercises to 1 set in CircuitItem and WorkoutBuilderBlocks

--- src/app/programs/components/workoutBuilder/CircuitItem.tsx
+++ src/app/programs/components/workoutBuilder/CircuitItem.tsx
@@ -79,9 +79,9 @@ const CircuitItem: React.FC<Props> = ({
     measurement: { measurementType: MeasurementType.REPS, measurementUnit: MeasurementUnit.POUND },
     notes: '',
     order,
-    sets: 3,
+    sets: 1,
     hasSuperset: false,
-    setData: Array.from({ length: 3 }, () => ({ reps: 10, weight: 0, rest: 0 })),
+    setData: [{ reps: 10, weight: 0, rest: 0 }],
   });
 
   const promoteToGroup = () => {
